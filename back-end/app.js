e9913b507ede33808dd1c23b7a431ccb6da81321
Nathalie Berwa <nathalieberwa@outlook.fr>
2023-09-29 04:12:38 +0200

    Add connection to the database

--- back-end/app.js
+++ back-end/app.js
@@ -1,8 +1,19 @@
+const bodyPerser = require("body-parser");
 const express = require("express");
-
+const mongoose = require("mongoose");
+require("dotenv").config();
 const app = express();
-const port = 8080;
+app.use(bodyPerser.json());
 
-app.listen(port, () => {
-  console.log(`Listen on port : ${port}`);
-});
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.MONGODB_USER}:${process.env.PASSWORD_MONGODB}@cluster0.ybifg1p.mongodb.net/?retryWrites=true&w=majority&appName=AtlasApp`
+  )
+  .then((res) => {
+    app.listen(process.env.PORT, () => {
+      console.log(`Listen on port : ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
