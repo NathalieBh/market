491ea4046e1ada731d61a66479143800f62f82ec
Nathalie Berwa <nathalieberwa@outlook.fr>
2023-09-29 22:31:17 +0200

    Add config for upload, images,security & graphQL

---
+++ back-end/controllers/authAndSponsor.js
@@ -0,0 +1,91 @@
+const { validationResult } = require('express-validator/check');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const User = require('../models/user');
+
+exports.signup = async (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    const error = new Error('Validation failed.');
+    error.statusCode = 422;
+    error.data = errors.array();
+    throw error;
+  }
+  const email = req.body.email;
+  const userId = req.body.userId;
+  try {
+    const user = new User({
+      email,
+      userId,
+    });
+    const result = await user.save();
+    res.status(201).json({ message: 'User created!', userId: result._id });
+  } catch (err) {
+    if (!err.statusCode) {
+      err.statusCode = 500;
+    }
+    next(err);
+  }
+};
+
+// Sessions & cokies
+
+exports.sponsored = async (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    const error = new Error('Validation failed.');
+    error.statusCode = 422;
+    error.data = errors.array();
+    throw error;
+  }
+  const sponsorId = req.params.sponsorId;
+  try {
+    const sponsor = await User.findOne({ userId: sponsorId });
+    if (sponsor) {
+      // create Cookies
+      res.status(201).json({ message: 'Sponsor found!', sponsor._id });
+    }
+
+  } catch (err) {
+    if (!err.statusCode) {
+      err.statusCode = 500;
+    }
+    next(err);
+  }
+};
+
+exports.login = async (req, res, next) => {
+  const email = req.body.email;
+  const userId = req.body.userId;
+  let loadedUser;
+  try {
+    const user = await User.findOne({ email: email });
+    if (!user) {
+      const error = new Error('A user with this email could not be found.');
+      error.statusCode = 401;
+      throw error;
+    }
+    loadedUser = user;
+    const isEqual = userId === user.userId;
+    if (!isEqual) {
+      const error = new Error('Wrong userId!');
+      error.statusCode = 401;
+      throw error;
+    }
+    const token = jwt.sign(
+      {
+        email: loadedUser.email,
+        userId: loadedUser._id.toString(),
+      },
+      'somesupersecretsecret',
+      { expiresIn: '1h' }
+    );
+    res.status(200).json({ token: token, userId: loadedUser._id.toString() });
+  } catch (err) {
+    if (!err.statusCode) {
+      err.statusCode = 500;
+    }
+    next(err);
+  }
+};
